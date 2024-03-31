80b247941870fe404ae4d6d163cd3f53dab4649b
pritamcn <you@example.com>
2024-03-31 21:50:44 +0530

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -27,6 +27,10 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <h1 className="text-3xl font-bold underline text-red-700">
+      Hello world!
+    </h1>
+
     </div>
   );
 }
